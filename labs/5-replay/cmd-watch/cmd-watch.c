03be4b00ec0585431974e44ecd8957428166f46b
Peter Moroz <ptr.moroz@gmail.com>
2023-06-24 21:06:47 +0300

    lab5: implement prelab/part 1 (cmd-watch)

--- labs/5-replay/cmd-watch/cmd-watch.c
+++ labs/5-replay/cmd-watch/cmd-watch.c
@@ -36,7 +36,37 @@ int check_activity(void) {
 
     static time_t last_mtime;   // store last modification time.
 
-    unimplemented();
+    DIR *dir = opendir(dirname);
+    if (dir == NULL)
+        panic("opendir() failed.\n");
+
+    struct dirent *entry = NULL;
+    struct stat fstat;
+    while ((entry = readdir(dir))) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+
+        const char *dot = strrchr(entry->d_name, '.');
+        if (dot == NULL)
+            dot = entry->d_name;
+        char **s = suffixes;
+        
+        for (; *s; s++) {
+            if (strcmp(*s, dot) == 0)
+                break;
+        }
+
+        if (*s == 0)
+            continue;
+
+        if (stat(entry->d_name, &fstat) < 0)
+            panic("stat() failed.\n");
+
+        if (fstat.st_mtime > last_mtime) {
+            last_mtime = fstat.st_mtime;
+            changed_p = 1;
+        }
+    }
 
     // return 1 if anything that matched <suffixes>
     return changed_p;
@@ -45,12 +75,15 @@ int check_activity(void) {
 // synchronously wait for <pid> to exit.  returns 1 if it exited
 // cleanly (via exit(0)), 0 otherwise.
 static int pid_clean_exit(int pid) {
-    unimplemented();
+    int status = -1;
+    if (waitpid(pid, &status, 0) < 0)
+        panic("waitpid() failed.\n");
+    return WIFEXITED(status) != 0 ? 1 : 0;
 }
 
 // simple helper to print null terminated vector of strings.
 static void print_argv(char *argv[]) {
-    assert(argv[0]);
+  assert(argv[0]);
 
 	fprintf(stderr, "about to exec =<%s ", argv[0]);
 	for(int i =1; argv[i]; i++)
@@ -63,13 +96,32 @@ static void print_argv(char *argv[]) {
 // and exit if the kid crashed or exited with an error (a non-zero
 // exit code).
 static void run(char *argv[]) {
-    unimplemented();
+    assert(argv[0]);
+    pid_t pid = fork();
+    if (pid < 0)
+        panic("fork() failed.\n");
+
+    if (pid == 0) {
+        if (execvp(argv[0], argv) < 0)
+            panic("execvp() failed.\n");
+    } else {
+        if (pid_clean_exit(pid) == 0)
+            die("the child process terminated ubnormally.\n");
+    }
 }
 
 int main(int argc, char *argv[]) {
     if(argc < 2)
         die("cmd-watch: not enough arguments\n");
-        
-    unimplemented();
+
+    while (1) {
+        if (usleep(250000) < 0)
+            panic("usleep() failed.\n");
+
+        if (check_activity()) {
+            run(argv + 1);
+        }
+    }
+    
     return 0;
 }
